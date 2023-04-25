959bcb2ff590388ca539faac95b32e647edea771
Guillermo Aviles <gjaviles7@gmail.com>
2023-04-24 20:27:51 -0400

    ui effects

--- robo-nouns/src/components/Footer.jsx
+++ robo-nouns/src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <div className="flex bg-nouns-lime">
             <footer className="Footer_footerSignature__wnClW font-press text-lg">
                 <a
-                    className="Link_link__DLONq"
+                    className="Link_link__DLONq hover:scale-105"
                     href="https://prop.house/nouns/hack-week-developers-developers-developers/5304"
                     target="_blank"
                     rel="noreferrer"
@@ -11,7 +11,7 @@ const Footer = () => {
                     Proposal
                 </a>
                 <a
-                    className="Link_link__DLONq"
+                    className="Link_link__DLONq hover:scale-105"
                     href="https://twitter.com/nounsdao"
                     target="_blank"
                     rel="noreferrer"
@@ -19,7 +19,7 @@ const Footer = () => {
                     Twitter
                 </a>
                 <a
-                    className="Link_link__DLONq"
+                    className="Link_link__DLONq hover:scale-105"
                     href="/"
                     target="_blank"
                     rel="noreferrer"
@@ -27,7 +27,7 @@ const Footer = () => {
                     Etherscan
                 </a>
                 <a
-                    className="Link_link__DLONq"
+                    className="Link_link__DLONq hover:scale-105"
                     href="https://discourse.nouns.wtf/"
                     target="_self"
                     rel="noreferrer"
@@ -35,7 +35,7 @@ const Footer = () => {
                     Forum
                 </a>
                 <a
-                    className="Link_link__DLONq"
+                    className="Link_link__DLONq hover:scale-105"
                     href="https://github.com/guillermoaviles/robo-nouns/tree/main/robo-nouns"
                     target="_self"
                     rel="noreferrer"
