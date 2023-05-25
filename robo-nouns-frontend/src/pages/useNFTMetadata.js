c535205a8d8009d0e4cf4b77dd7418262b1efb8e
Guillermo Aviles <gjaviles7@gmail.com>
2023-05-25 22:41:08 +0300

    new contract

--- robo-nouns-frontend/src/pages/useNFTMetadata.js
+++ robo-nouns-frontend/src/pages/useNFTMetadata.js
@@ -1,57 +1,44 @@
 import { useEffect, useState } from "react";
 import { useAuction } from "../../src/context/AuctionContext";
 import { ethers } from "ethers";
-import addresses from "../../src/utils/addresses.json";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk/evm";
 
-const contractAddress = "0xA75E74a5109Ed8221070142D15cEBfFe9642F489";
-const contractAbi = addresses.RoboNounsVRGDA.abi;
+const sdk = new ThirdwebSDK("mumbai");
 
 export function useNFTMetadata() {
-    const { addNounData, 
-            setMinMintPrice, 
-            setCurrMintPrice, 
-            setTargetMintPrice 
-        } = useAuction();
-        
-    const [roboNoun, setRoboNoun] = useState("");
+  const { addNounData, setMinMintPrice, setCurrMintPrice, setTargetMintPrice } = useAuction();
+  const [roboNoun, setRoboNoun] = useState("");
 
-    useEffect(() => {
-        async function fetchNFTMetadata() {
-            const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
-            const signer = provider.getSigner();
-            const contract = new ethers.Contract(contractAddress, contractAbi, signer);
-
-            const nounMeta = await contract.fetchNextNoun();
-            const { data: currentPrice, isLoading: loadingCurr } = useContractRead(
-                contract,
-                "getCurrentVRGDAPrice"
-            )
-            const { data: minPrice, isLoading: loadingMin } = useContractRead(
-                contract,
-                "reservePrice"
-            )
-            const { data: targetPrice, isLoading: loadingTarget } = useContractRead(
-                contract,
-                "targetPrice"
-            )
-            const minMintPrice = loadingMin ? "" : ethers.utils.formatEther(minPrice)
-            const currMintPrice = loadingCurr ? "" : ethers.utils.formatEther(currentPrice)
-            const targetMintPrice = loadingTarget ? "" : ethers.utils.formatEther(targetPrice)
-            console.log('currMintPrice', currMintPrice)
-            const newRoboNoun = nounMeta
-                ? `data:image/svg+xml;base64,${nounMeta.svg}`
-                : "";
-
-            setRoboNoun(newRoboNoun);
-            addNounData(nounMeta);
-            setMinMintPrice(minMintPrice)
-            setCurrMintPrice(currMintPrice)
-            setTargetMintPrice(targetMintPrice)
+  useEffect(() => {
+    async function fetchNFTMetadata() {
+      try {
+        if (!sdk.isConnected()) {
+          await sdk.connect(); // Connect to the network if not already connected
         }
 
-        const interval = setInterval(fetchNFTMetadata, 1000);
-        return () => clearInterval(interval);
-    }, [addNounData, setMinMintPrice, setCurrMintPrice, setTargetMintPrice ]);
+        const contract = await sdk.getContract("0x255414e3d2cf6D316776b5E1dD1e66925144232D");
+        const nounMeta = await contract.call("fetchNextNoun");
+        const currentPrice = await contract.call("getCurrentVRGDAPrice");
+        const minPrice = await contract.call("reservePrice");
+        const targetPrice = await contract.call("targetPrice");
+        const minMintPrice = ethers.utils.formatEther(minPrice);
+        const currMintPrice = ethers.utils.formatEther(currentPrice);
+        const targetMintPrice = ethers.utils.formatEther(targetPrice);
 
-    return roboNoun;
+        const newRoboNoun = nounMeta ? `data:image/svg+xml;base64,${nounMeta.svg}` : "";
+        setRoboNoun(newRoboNoun);
+        addNounData(nounMeta);
+        setMinMintPrice(minMintPrice);
+        setCurrMintPrice(currMintPrice);
+        setTargetMintPrice(targetMintPrice);
+      } catch (error) {
+        console.error("Error fetching NFT metadata:", error);
+      }
+    }
+
+    const interval = setInterval(fetchNFTMetadata, 1000);
+    return () => clearInterval(interval);
+  }, [addNounData, setMinMintPrice, setCurrMintPrice, setTargetMintPrice]);
+
+  return roboNoun;
 }
