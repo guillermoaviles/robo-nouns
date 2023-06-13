784e45a9e9558b6f6d2cebdad1c12b2f0128cb69
Guillermo Aviles <gjaviles7@gmail.com>
2023-06-12 23:08:22 -0400

    swapped to Alchemy for fetchNextNoun

--- robo-nouns-frontend/src/context/AuctionContext.jsx
+++ robo-nouns-frontend/src/context/AuctionContext.jsx
@@ -5,79 +5,77 @@ import { ethers } from "ethers";
 const AuctionContext = createContext();
 
 export function useAuction() {
-	return useContext(AuctionContext);
+  return useContext(AuctionContext);
 }
 
 export function AuctionProvider({ children }) {
-	const [nounNFTMeta, setNounNFTMeta] = useState([]);
-	const [minMintPrice, setMinMintPrice] = useState("");
-	const [currMintPrice, setCurrMintPrice] = useState("");
-	const [targetMintPrice, setTargetMintPrice] = useState("");
+  const [nounNFTMeta, setNounNFTMeta] = useState([]);
+  const [minMintPrice, setMinMintPrice] = useState("");
+  const [currMintPrice, setCurrMintPrice] = useState("");
+  const [targetMintPrice, setTargetMintPrice] = useState("");
 
-	const { contract } = useContract(
-		"0x402CF8456F05D111ABEe84F4209334Ba37729479"
-	);
+  const providerUrl = "https://eth-goerli.g.alchemy.com/v2/8kIFZ8iBRuBDAQqIH73BfPB8ESBwbIUt";
+  const provider = new ethers.providers.JsonRpcProvider(providerUrl);
 
-	const fetchNFTMetadata = async () => {
-		try {
-			const nounMeta = await contract?.call("fetchNextNoun");
-			const newRoboNoun = nounMeta
-				? `data:image/svg+xml;base64,${nounMeta.svg}`
-				: "";
-			addNounData(nounMeta);
+  const auctionContractAddress = "0x402CF8456F05D111ABEe84F4209334Ba37729479";
+ 
+  const auctionContractABI = JSON.parse("[{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"_reservePrice\",\"type\":\"uint256\"},{\"internalType\":\"int256\",\"name\":\"_targetPrice\",\"type\":\"int256\"},{\"internalType\":\"uint256\",\"name\":\"_priceDecayPercent\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"_perTimeUnit\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"_updateInterval\",\"type\":\"uint256\"},{\"internalType\":\"address\",\"name\":\"_token\",\"type\":\"address\"}],\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"perTimeUnit\",\"type\":\"uint256\"}],\"name\":\"AuctionPerTimeUnitUpdated\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"priceDecayPercent\",\"type\":\"uint256\"}],\"name\":\"AuctionPriceDecayPercentUpdated\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"reservePrice\",\"type\":\"uint256\"}],\"name\":\"AuctionReservePriceUpdated\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"uint256\",\"name\":\"nounId\",\"type\":\"uint256\"},{\"indexed\":false,\"internalType\":\"address\",\"name\":\"winner\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"AuctionSettled\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":false,\"internalType\":\"int256\",\"name\":\"targetPrice\",\"type\":\"int256\"}],\"name\":\"AuctionTargetPriceUpdated\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"updateInterval\",\"type\":\"uint256\"}],\"name\":\"AuctionUpdateIntervalUpdated\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"previousOwner\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"OwnershipTransferred\",\"type\":\"event\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"expectedBlockNumber\",\"type\":\"uint256\"}],\"name\":\"buyNow\",\"outputs\":[],\"stateMutability\":\"payable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"decayConstant\",\"outputs\":[{\"internalType\":\"int256\",\"name\":\"\",\"type\":\"int256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"fetchNextNoun\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"nounId\",\"type\":\"uint256\"},{\"components\":[{\"internalType\":\"uint48\",\"name\":\"background\",\"type\":\"uint48\"},{\"internalType\":\"uint48\",\"name\":\"body\",\"type\":\"uint48\"},{\"internalType\":\"uint48\",\"name\":\"accessory\",\"type\":\"uint48\"},{\"internalType\":\"uint48\",\"name\":\"head\",\"type\":\"uint48\"},{\"internalType\":\"uint48\",\"name\":\"glasses\",\"type\":\"uint48\"}],\"internalType\":\"struct INounsSeeder.Seed\",\"name\":\"seed\",\"type\":\"tuple\"},{\"internalType\":\"string\",\"name\":\"svg\",\"type\":\"string\"},{\"internalType\":\"uint256\",\"name\":\"price\",\"type\":\"uint256\"},{\"internalType\":\"bytes32\",\"name\":\"hash\",\"type\":\"bytes32\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"getCurrentVRGDAPrice\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"lastTokenBlock\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"nounsDAO\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"owner\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"perTimeUnit\",\"outputs\":[{\"internalType\":\"int256\",\"name\":\"\",\"type\":\"int256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"renounceOwnership\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"reservePrice\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"roboNounsToken\",\"outputs\":[{\"internalType\":\"contract RoboNounsToken\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"_perTimeUnit\",\"type\":\"uint256\"}],\"name\":\"setPerTimeUnit\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"_priceDecayPercent\",\"type\":\"uint256\"}],\"name\":\"setPriceDecayPercent\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"_reservePrice\",\"type\":\"uint256\"}],\"name\":\"setReservePrice\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"int256\",\"name\":\"_targetPrice\",\"type\":\"int256\"}],\"name\":\"setTargetPrice\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"_updateInterval\",\"type\":\"uint256\"}],\"name\":\"setUpdateInterval\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"startTime\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"targetPrice\",\"outputs\":[{\"internalType\":\"int256\",\"name\":\"\",\"type\":\"int256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"transferOwnership\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"updateInterval\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]")
 
-			const currentPrice = await contract?.call("getCurrentVRGDAPrice");
-			const minPrice = await contract?.call("reservePrice");
-			const targetPrice = await contract?.call("targetPrice");
-			const minMintPrice = minPrice ? ethers.utils.formatEther(minPrice) : "";
-			const currMintPrice = currentPrice
-				? ethers.utils.formatEther(currentPrice)
-				: "";
-			const targetMintPrice = targetPrice
-				? ethers.utils.formatEther(targetPrice)
-				: "";
+  const contract = new ethers.Contract(auctionContractAddress, auctionContractABI, provider);
 
-			setMinMintPrice(minMintPrice);
-			setCurrMintPrice(currMintPrice);
-			setTargetMintPrice(targetMintPrice);
-		} catch (error) {
-			console.error("Error fetching NFT metadata and price info:", error);
-		}
-	};
+  const fetchNFTMetadata = async () => {
+    try {
+      const nounMeta = await contract.fetchNextNoun();
+      addNounData(nounMeta);
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			fetchNFTMetadata();
-		}, 1000);
-		return () => clearInterval(interval);
-	}, [nounNFTMeta, contract]);
+      const currentPrice = await contract.getCurrentVRGDAPrice();
+      const minPrice = await contract.reservePrice();
+      const targetPrice = await contract.targetPrice();
+      const minMintPrice = minPrice ? ethers.utils.formatEther(minPrice) : "";
+      const currMintPrice = currentPrice ? ethers.utils.formatEther(currentPrice) : "";
+      const targetMintPrice = targetPrice ? ethers.utils.formatEther(targetPrice) : "";
 
-	const addNounData = (newNoun) => {
-		const isDuplicate = nounNFTMeta
-			? nounNFTMeta.some((noun) => noun?.hash === newNoun?.hash)
-			: false;
-		if (!isDuplicate) {
-			const updatedNounNFTMeta = [newNoun, ...nounNFTMeta];
-			if (updatedNounNFTMeta.length > 4) {
-				updatedNounNFTMeta.pop();
-			}
-			setNounNFTMeta(updatedNounNFTMeta);
-		}
-	};
+      setMinMintPrice(minMintPrice);
+      setCurrMintPrice(currMintPrice);
+      setTargetMintPrice(targetMintPrice);
+    } catch (error) {
+      console.error("Error fetching NFT metadata and price info:", error);
+    }
+  };
 
-	const value = {
-		addNounData,
-		nounNFTMeta,
-		setNounNFTMeta,
-		minMintPrice,
-		setMinMintPrice,
-		currMintPrice,
-		setCurrMintPrice,
-		targetMintPrice,
-		setTargetMintPrice,
-	};
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchNFTMetadata();
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [nounNFTMeta, contract]);
 
-	return (
-		<AuctionContext.Provider value={value}>{children}</AuctionContext.Provider>
-	);
+  const addNounData = (newNoun) => {
+    const isDuplicate = nounNFTMeta ? nounNFTMeta.some((noun) => noun?.hash === newNoun?.hash) : false;
+    if (!isDuplicate) {
+      const updatedNounNFTMeta = [newNoun, ...nounNFTMeta];
+      if (updatedNounNFTMeta.length > 4) {
+        updatedNounNFTMeta.pop();
+      }
+      setNounNFTMeta(updatedNounNFTMeta);
+    }
+  };
+
+  const value = {
+    addNounData,
+    nounNFTMeta,
+    setNounNFTMeta,
+    minMintPrice,
+    setMinMintPrice,
+    currMintPrice,
+    setCurrMintPrice,
+    targetMintPrice,
+    setTargetMintPrice,
+  };
+
+  return (
+    <AuctionContext.Provider value={value}>
+      {children}
+    </AuctionContext.Provider>
+  );
 }
