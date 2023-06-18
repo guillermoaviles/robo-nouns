6c87f21bb8cfd33769dd270d6a3b1ba68872b501
Guillermo Aviles <gjaviles7@gmail.com>
2023-06-18 04:53:34 -0400

    ui changes

--- robo-nouns-frontend/src/components/PrevNFTs/index.jsx
+++ robo-nouns-frontend/src/components/PrevNFTs/index.jsx
@@ -12,14 +12,14 @@ const PrevNFTs = () => {
             <h2 className="font-press text-center text-white text-2xl pt-4 md:pt-16">
                 Last 3 blocks:
             </h2>
-            <div className="flex flex-col md:flex-row p-6 justify-evenly">
+            <div className="flex flex-col md:flex-row p-6 justify-center">
                 {nounNFTMeta &&
                     nounNFTMeta.slice(-3).map((nft, index) => (
                         <div key={index} className="py-6">
                             <Image
                                 src={nft?.svg ? `data:image/svg+xml;base64,${nft?.svg}` : loadingNoun}
                                 alt={`NFT ${index}`}
-                                width={212}
+                                width={280}
                                 height={200}
                             />
                             <div className="justify-center mt-10">
