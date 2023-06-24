66be0d889fb59dff72958c71973d42bcb2f86dd7
vbazhutin <vladimir.bazhutin@gmail.com>
2023-06-24 02:25:20 -0400

    prettier

---
+++ robo-nouns-frontend/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    semi: false,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    arrowParens: "always",
+    bracketSameLine: false,
+    printWidth: 80,
+    useTabs: false,
+}
