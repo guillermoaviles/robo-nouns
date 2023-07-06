c97886b0eae94e019b49bf96db937737d5866396
Guillermo Aviles <gjaviles7@gmail.com>
2023-07-05 23:44:34 -0400

    initial refactor

--- robo-nouns-frontend/src/components/NounImg/index.jsx
+++ robo-nouns-frontend/src/components/NounImg/index.jsx
@@ -1,23 +1,20 @@
-import Image from "next/image"
-import { useAuction } from "../../context/AuctionContext"
-import loadingNoun from "../../assets/loading-skull-noun.gif"
+import Image from "next/image";
+import { useAuction } from "../../context/AuctionContext";
+import loadingNoun from "../../assets/loading-skull-noun.gif";
 
-export default function NounImg() {
-    const { nounNFTMeta } = useAuction()
-    
-    return (
-        <div className="flex w-full lg:w-1/2">
-            <div className="w-full relative">
-                <div className="md:h-0 w-full">
-                    <Image
-                        src={nounNFTMeta[0]?.svg ? `data:image/svg+xml;base64,${nounNFTMeta[0]?.svg}` : loadingNoun}
-                        alt="Noun Image"
-                        className="md:absolute top-0 left-0 w-full md:h-full"
-                        width={300}
-                        height={300}
-                    />
-                </div>
-            </div>
+export default function NounImg({ nounNFTMeta }) {
+  return (
+    <div className="w-full lg:w-1/2">
+      <div className="relative aspect-w-1 aspect-h-1">
+        <div className="absolute inset-0">
+          <Image
+            src={nounNFTMeta[0]?.svg ? `data:image/svg+xml;base64,${nounNFTMeta[0]?.svg}` : loadingNoun}
+            alt="Noun Image"
+            className="object-cover w-full h-full"
+            layout="fill"
+          />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
